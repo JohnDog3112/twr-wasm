ead54956791bd8cde23ff501a8796b4797e4d40a
Johnathon <johnathonmgaines@gmail.com>
2025-04-11 15:11:49 -0500

    Pushing batch of changes done in screen

--- source/twr-ts/twrconscreen.ts
+++ source/twr-ts/twrconscreen.ts
@@ -182,6 +182,8 @@ interface WindowInfo {
    cursor: string,
    popupWindows: DoublyLinkedListRoot<WindowInfo>,
    parent: WindowParent,
+
+   childSpawnOffset: WindowSpawnOffset,
    // selectedPopupWindow: boolean,
 };
 interface ResizeInfo {
@@ -198,6 +200,12 @@ interface DragInfo {
    startY: number,
    windowStartX: number,
    windowStartY: number,
+}
+interface WindowSpawnOffset {
+   x: number,
+   y: number,
+   baseX: number,
+   lastSpawnTime: number
 }
 enum BroadCursorState {
    Background,
@@ -221,6 +229,11 @@ function areCursorStatesEqual(a: CursorState, b: CursorState): boolean {
       case BroadCursorState.Resizing:
          return a[1] == b[1];
    }
+}
+interface LastHoveredWindow {
+   window: WindowInfo,
+   lastX: number,
+   lastY: number,
 }
 export class twrConsoleScreen extends twrLibrary implements ICanvasEvents {
    id: number;
@@ -301,12 +314,15 @@ export class twrConsoleScreen extends twrLibrary implements ICanvasEvents {
       x: 50,
       y: 50
    };
-   private windowSpawnOffset = {
+   private lastInteractionTime = Date.now();
+   private windowSpawnOffset: WindowSpawnOffset = {
       x: this.baseWindowSpaceOffset.x,
       y: this.baseWindowSpaceOffset.y,
       //the base x coordinate at the top
       // is incremented by spawnOffsetChange everytime windows loop back to the top
       baseX: this.baseWindowSpaceOffset.x,
+      
+      lastSpawnTime: this.lastInteractionTime + 10,
    };
    private static internalChildDragFunction(weakThis: WeakRef<twrConsoleScreen>, weakWindowInfo: WeakRef<WindowInfo>, x: number, y: number, event: CanvasEventTypes) {
       const strongThis = weakThis.deref();
@@ -375,10 +391,10 @@ export class twrConsoleScreen extends twrLibrary implements ICanvasEvents {
    }
    //how much to change windowSpawnOffset every time a window is spawned
    readonly spawnOffsetChange = 25;
-   jsSpawnWindow(): twrConsoleWindow {
+   jsSpawnWindow(width: number = 500, height: number = 500, x?: number, y?: number): twrConsoleWindow {
       const canvas = document.createElement("canvas");
-      canvas.height = 500;
-      canvas.width = 500;
+      canvas.height = width;
+      canvas.width = height;
       if (this.windowSpawnOffset.x >= this.canvas.width-50) {
          this.windowSpawnOffset.x = this.canvas.width > 100 ? this.baseWindowSpaceOffset.x : 5;
          this.windowSpawnOffset.y = this.canvas.height > 100 ? this.baseWindowSpaceOffset.y : 5;
@@ -389,13 +405,17 @@ export class twrConsoleScreen extends twrLibrary implements ICanvasEvents {
          this.windowSpawnOffset.baseX += this.spawnOffsetChange;
          this.windowSpawnOffset.x = this.windowSpawnOffset.baseX;
       }
+      if (this.lastInteractionTime > this.windowSpawnOffset.lastSpawnTime) {
+         this.windowSpawnOffset.x = this.baseWindowSpaceOffset.x;
+         this.windowSpawnOffset.y = this.baseWindowSpaceOffset.y;
+      }
 
       const windowInfo: WindowInfo = {
          //small hack to allow WindowInfo to be defined before window
          // so it can be passed into the handler functions like drag, resize, and cursor
          window: undefined as any as twrConsoleWindow,
-         x: this.windowSpawnOffset.x,
-         y: this.windowSpawnOffset.y,
+         x: x ?? this.windowSpawnOffset.x,
+         y: y ?? this.windowSpawnOffset.y,
          hidden: false,
          minXSize: 200,
          minYSize: 200,
@@ -403,8 +423,14 @@ export class twrConsoleScreen extends twrLibrary implements ICanvasEvents {
          popupWindows: new DoublyLinkedListRoot(),
          parent: [WindowParentType.Layer, 1],
          // selectedPopupWindow: false,
+         childSpawnOffset: {
+            x: 0,
+            y: 0,
+            baseX: 0,
+            lastSpawnTime: Date.now(),
+         }
       };
-      let weakWindowInfo = new WeakRef(windowInfo);
+      const weakWindowInfo = new WeakRef(windowInfo);
       const weakThis = new WeakRef(this);
       
       const window = new twrConsoleWindow(
@@ -418,7 +444,6 @@ export class twrConsoleScreen extends twrLibrary implements ICanvasEvents {
       
       this.windowSpawnOffset.x += this.spawnOffsetChange;
       this.windowSpawnOffset.y += this.spawnOffsetChange;
-      weakWindowInfo = new WeakRef(windowInfo);
       this.windows.set(window.id, windowInfo);
       const node = this.windowOrder[1].createNodeAtFront(windowInfo);
       // this.selectedWindowLayer = 1;
@@ -428,9 +453,20 @@ export class twrConsoleScreen extends twrLibrary implements ICanvasEvents {
       return window;
    }
 
-   
-   twrScreenSpawnWindow(mod: IWasmModule | IWasmModuleAsync): number {
-      const window = this.jsSpawnWindow();
+   private internalSetNegativesUndefined(val: number|undefined): number|undefined {
+      if (val == undefined || val < 0) {
+         return undefined;
+      } else {
+         return val;
+      }
+   }
+   twrScreenSpawnWindow(mod: IWasmModule | IWasmModuleAsync, width?: number, height?: number, x?: number, y?: number): number {
+      const window = this.jsSpawnWindow(
+         this.internalSetNegativesUndefined(width), 
+         this.internalSetNegativesUndefined(height), 
+         this.internalSetNegativesUndefined(x),
+         this.internalSetNegativesUndefined(y)
+      );
       return window.id;
    }
 
@@ -450,6 +486,165 @@ export class twrConsoleScreen extends twrLibrary implements ICanvasEvents {
       const windowInfo = this.windows.get(window);
       if (windowInfo == undefined) throw new Error(`twrScreenSetWindowLayer was given a window not registered with this screen!`);
       this.internalSetWindowLayer(windowInfo, layer);
+   }
+
+   jsSpawnPopupWindow(window: twrConsoleWindow, width: number = 50, height: number = 50, x?: number, y?: number): twrConsoleWindow {
+      const windowInfo = this.windows.get(window.id);
+      if (windowInfo == undefined) throw new Error(`twrScreenSpawnPopupWindow was given a window not registered with this screen!`);
+      
+      const canvas = document.createElement("canvas");
+      canvas.height = width;
+      canvas.width = height;
+
+      const middleX = windowInfo.x + (windowInfo.window.element.width - width)/2;
+      const middleY = windowInfo.y + (windowInfo.window.element.height - height)/2;
+      
+      const childSpawnOffset = this.windowSpawnOffset;
+      if (childSpawnOffset.x+middleX >= this.canvas.width-50) {
+         childSpawnOffset.x = 0;
+         childSpawnOffset.y = 0;
+         childSpawnOffset.baseX = childSpawnOffset.x;
+      }
+      if (childSpawnOffset.y+middleY >= this.canvas.height-50) {
+         childSpawnOffset.y = 0;
+         childSpawnOffset.baseX += this.spawnOffsetChange;
+         childSpawnOffset.x = childSpawnOffset.baseX;
+      }
+      if (this.lastInteractionTime > childSpawnOffset.lastSpawnTime) {
+         childSpawnOffset.x = 0;
+         childSpawnOffset.y = 0;
+      }
+      const offsetSpawnX = middleX + childSpawnOffset.x;
+      const offsetSpawnY = middleY + childSpawnOffset.y;
+      
+      let spawnX = offsetSpawnX+width;
+      if (offsetSpawnX <= 0) {
+         spawnX = 0;
+      } else if (offsetSpawnX+width >= this.canvas.width) {
+         spawnX = this.canvas.width-width;
+      }
+      let spawnY = offsetSpawnY+height;
+      if (offsetSpawnY <= 0) {
+         spawnY = 0;
+      } else if (offsetSpawnY+height >= this.canvas.height) {
+         spawnY = this.canvas.height-height;
+      }
+
+      const popupWindowInfo: WindowInfo = {
+         //small hack to allow WindowInfo to be defined before window
+         // so it can be passed into the handler functions like drag, resize, and cursor
+         window: undefined as any as twrConsoleWindow,
+         x: x ?? spawnX,
+         y: y ?? spawnY,
+         hidden: false,
+         minXSize: width,
+         minYSize: height,
+         cursor: "auto",
+         popupWindows: new DoublyLinkedListRoot(),
+         parent: [WindowParentType.Window, windowInfo],
+         childSpawnOffset: {
+            x: 0,
+            y: 0,
+            baseX: 0,
+            lastSpawnTime: Date.now(),
+         }
+      };
+      let weakPopupInfo = new WeakRef(popupWindowInfo);
+      const weakThis = new WeakRef(this);
+      
+      const popupWindow = new twrConsoleWindow(
+         canvas, 
+         false, 
+         twrConsoleScreen.internalChildDragFunction.bind(undefined, weakThis, weakPopupInfo), 
+         twrConsoleScreen.internalChildResizeFunction.bind(undefined, weakThis, weakPopupInfo), 
+         twrConsoleScreen.internalChildSetCursor.bind(undefined, weakThis, weakPopupInfo)
+      );
+      popupWindowInfo.window = popupWindow;
+      
+      childSpawnOffset.x += this.spawnOffsetChange;
+      childSpawnOffset.y += this.spawnOffsetChange;
+      this.windows.set(popupWindow.id, windowInfo);
+      const node = windowInfo.popupWindows.createNodeAtFront(windowInfo);
+      this.selectedWindow = new WeakRef(node.val);
+      popupWindowInfo.orderNode = node;
+
+      return popupWindow;
+   }
+   twrScreenSpawnPopupWindow(mod: IWasmModule | IWasmModuleAsync, windowID: number, width?: number, height?: number, x?: number, y?: number) {
+      const windowInfo = this.windows.get(windowID);
+      if (windowInfo == undefined) throw new Error(`twrScreenSpawnPopupWindow was given a window not registered with this screen!`);
+      const popupWindow = this.jsSpawnPopupWindow(
+         windowInfo.window,
+         this.internalSetNegativesUndefined(width),
+         this.internalSetNegativesUndefined(height),
+         this.internalSetNegativesUndefined(x),
+         this.internalSetNegativesUndefined(y),
+      );
+
+      return popupWindow.id;
+   }
+
+   private internalCloseWindow(windowInfo: WindowInfo) {
+      this.internalDeselectWindow(windowInfo);
+
+      windowInfo.parent = [WindowParentType.Layer, -1];
+
+      this.windows.delete(windowInfo.window.id);
+      windowInfo.orderNode!.cutConnections();
+      
+      for (let node = windowInfo.popupWindows.getRoot(); node != undefined; node = node.getNext()) {
+         this.internalCloseWindow(node.val);
+      }
+   }
+
+   jsCloseWindow(window: twrConsoleWindow) {
+      const windowInfo = this.windows.get(window.id);
+      if (windowInfo == undefined) throw new Error(`jsCloseWindow was given a window not registered with this screen!`);
+      
+      this.internalCloseWindow(windowInfo);
+   }
+   twrScreenCloseWindow(mod: IWasmModule | IWasmModuleAsync, windowID: number) {
+      const windowInfo = this.windows.get(windowID);
+      if (windowInfo == undefined) throw new Error(`twrScreenCloseWindow was given a window not registered with this screen!`);
+
+      this.internalCloseWindow(windowInfo);
+   }
+
+   private internalMinimizeWindow(window: WindowInfo) {
+      if (!window.hidden) {
+         this.internalDeselectWindow(window);
+         window.hidden = true;
+      }
+   }
+   jsMinimizeWindow(window: twrConsoleWindow) {
+      const windowInfo = this.windows.get(window.id);
+      if (windowInfo == undefined) throw new Error(`jsMinimizeWindow was given a window not registered with this screen!`);
+
+      this.internalMinimizeWindow(windowInfo);
+   }
+   twrScreenMinimizeWindow(mod: IWasmModule | IWasmModuleAsync, windowID: number) {
+      const windowInfo = this.windows.get(windowID);
+      if (windowInfo == undefined) throw new Error(`twrScreenMinimizeWindow was given a window id not registered with this screen!`);
+
+      this.internalMinimizeWindow(windowInfo);
+   }
+
+   private internalUnminimizeWindow(window: WindowInfo) {
+      if (window.hidden) {
+         window.hidden = false;
+      }
+   }
+   jsUnminimizeWindow(window: twrConsoleWindow) {
+      const windowInfo = this.windows.get(window.id);
+      if (windowInfo == undefined) throw new Error(`jsUniminimizeWindow was given a window not registered with this screen!`);
+
+      this.internalUnminimizeWindow(windowInfo);
+   }
+   twrScreenUnminimizeWindow(mod: IWasmModule | IWasmModuleAsync, windowID: number) {
+      const windowInfo = this.windows.get(windowID);
+      if (windowInfo == undefined) throw new Error(`twrScreenUnminimizeWindow was given a window id not registered with this screen!`);
+
+      this.internalUnminimizeWindow(windowInfo);
    }
 
 
@@ -508,7 +703,26 @@ export class twrConsoleScreen extends twrLibrary implements ICanvasEvents {
          }
       }
    }
-   private lastHoveredWindow?: WeakRef<WindowInfo>;
+   private lastHoveredWindow?: WeakRef<LastHoveredWindow>;
+   private internalDeselectWindow(windowInfo: WindowInfo) {
+      if (this.selectedWindow?.deref() == windowInfo) {
+         this.selectedWindow = undefined;
+         windowInfo.window.handleCanvasMouseEvent(CanvasEventTypes.MOUSE_CLICKED_OFF, -1, -1, -1);
+      }
+      if (this.lastHoveredWindow?.deref()?.window == windowInfo) {
+         const data = this.lastHoveredWindow!.deref()!;
+         this.lastHoveredWindow = undefined;
+         this.setCursorState([BroadCursorState.Background]);
+         windowInfo.window.handleCanvasMouseEvent(CanvasEventTypes.MOUSE_LEAVE, data.lastX, data.lastY, 0);
+      }
+      if (this.clickedWindow == windowInfo) {
+         this.clickedWindow.window.handleCanvasMouseEvent(CanvasEventTypes.MOUSE_UP, 0, 0, 0);
+
+         this.clickedWindow = undefined;
+         this.dragStart = undefined;
+         this.resizeStart = undefined;
+      }
+   }
    handleCanvasMouseEvent(event: CanvasEventTypes, x: number, y: number, button: number): boolean {
       this.mouseX = x;
       this.mouseY = y;
@@ -516,6 +730,7 @@ export class twrConsoleScreen extends twrLibrary implements ICanvasEvents {
 
       if (event == CanvasEventTypes.MOUSE_MOVE && this.resizeStart) {
          assertDefined(this.clickedWindow);
+         this.lastInteractionTime = Date.now();
          let nX: number|undefined = undefined;
          let nY: number|undefined = undefined;
          let nXSize: number|undefined = undefined;
@@ -583,6 +798,7 @@ export class twrConsoleScreen extends twrLibrary implements ICanvasEvents {
          }
          return true;
       } if (event == CanvasEventTypes.MOUSE_MOVE && this.dragStart) {
+         this.lastInteractionTime = Date.now();
          assertDefined(this.clickedWindow);
          this.moveWindow(
             this.clickedWindow.window,
@@ -591,6 +807,7 @@ export class twrConsoleScreen extends twrLibrary implements ICanvasEvents {
          );
          return true;
       } else if (event == CanvasEventTypes.MOUSE_MOVE && this.clickedWindow) {
+         this.lastInteractionTime = Date.now();
          this.clickedWindow.window.handleCanvasMouseEvent(
             event,
             x - this.clickedWindow.x,
@@ -599,6 +816,7 @@ export class twrConsoleScreen extends twrLibrary implements ICanvasEvents {
          );
          return true;
       } else if (event == CanvasEventTypes.MOUSE_UP && this.clickedWindow) {
+         this.lastInteractionTime = Date.now();
          this.clickedWindow.window.handleCanvasMouseEvent(
             event,
             x - this.clickedWindow.x,
@@ -612,12 +830,13 @@ export class twrConsoleScreen extends twrLibrary implements ICanvasEvents {
          this.handleCanvasMouseEvent(CanvasEventTypes.MOUSE_MOVE, x, y, button);
          return true;
       } else if (event == CanvasEventTypes.MOUSE_LEAVE) {
+         this.lastInteractionTime = Date.now();
          const lastWindow = this.lastHoveredWindow?.deref();
          if (lastWindow != undefined) {
-            lastWindow.window.handleCanvasMouseEvent(
+            lastWindow.window.window.handleCanvasMouseEvent(
                CanvasEventTypes.MOUSE_LEAVE,
-               x - lastWindow.x,
-               y - lastWindow.y,
+               lastWindow.lastX,
+               lastWindow.lastY,
                button
             );
          }
@@ -626,6 +845,16 @@ export class twrConsoleScreen extends twrLibrary implements ICanvasEvents {
 
          return true;
       } else if (event == CanvasEventTypes.MOUSE_CLICKED_OFF) {
+         this.lastInteractionTime = Date.now();
+         this.clickedWindow?.window.handleCanvasMouseEvent(
+            CanvasEventTypes.MOUSE_UP,
+            0,
+            0,
+            0
+         );
+         this.clickedWindow = undefined;
+         this.dragStart = undefined;
+         this.resizeStart = undefined;
          this.selectedWindow?.deref()?.window.handleCanvasMouseEvent(
             CanvasEventTypes.MOUSE_CLICKED_OFF,
             -1,
@@ -643,6 +872,7 @@ export class twrConsoleScreen extends twrLibrary implements ICanvasEvents {
       if (hoveredWindow == undefined) {
          this.setCursorState([BroadCursorState.Background]);
       } else {
+         this.lastInteractionTime = Date.now();
          switch (event) {
             case CanvasEventTypes.MOUSE_DOWN:
                this.clickedWindow = hoveredWindow;
@@ -671,17 +901,27 @@ export class twrConsoleScreen extends twrLibrary implements ICanvasEvents {
       const lastWindow = this.lastHoveredWindow?.deref();
       if (lastWindow != hoveredWindow) {
          if (lastWindow != undefined) {
-            lastWindow.window.handleCanvasMouseEvent(
+            lastWindow.window.window.handleCanvasMouseEvent(
                CanvasEventTypes.MOUSE_LEAVE,
-               x - lastWindow.x,
-               y - lastWindow.y,
+               x - lastWindow.window.x,
+               y - lastWindow.window.y,
                button
             );   
          }
-         if (hoveredWindow != undefined)
-            this.lastHoveredWindow = new WeakRef(hoveredWindow);
-         else
+         if (hoveredWindow != undefined) {
+            this.lastHoveredWindow = new WeakRef({
+               window: hoveredWindow, 
+               lastX: x - hoveredWindow.x, 
+               lastY: y - hoveredWindow.y
+            });
+         } else {
             this.lastHoveredWindow = undefined;
+         }
+      }
+      const updatedLastWindow = this.lastHoveredWindow?.deref();
+      if (updatedLastWindow != undefined) {
+         updatedLastWindow.lastX = x - updatedLastWindow.window.x;
+         updatedLastWindow.lastY = y - updatedLastWindow.window.y;
       }
       return true;
    }
@@ -689,13 +929,17 @@ export class twrConsoleScreen extends twrLibrary implements ICanvasEvents {
       if (this.clickedWindow)
          return true;
       const hoveredWindow = this.internalFindHoveredWindow(this.mouseX, this.mouseY);
-      if (hoveredWindow)
+      if (hoveredWindow) {
+         this.lastInteractionTime = Date.now();
          hoveredWindow.window.handleCanvasWheelEvent(event, deltaX, deltaY, deltaZ, deltaMode);
+      }
       
       return true;
    }
    private internalRenderWindows(event: CanvasEventTypes, delta: number, windows: DoublyLinkedListRoot<WindowInfo>) {
       for (let node = windows.getTail(); node != undefined; node = node.getPrev()) {
+         if (node.val.hidden) continue;
+
          node.val.window.handleCanvasAnimationFrameEvent(event, delta);
          this.ctx.drawImage(
             node.val.window.element,
