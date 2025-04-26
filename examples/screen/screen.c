2661635a7473897da8d320f7cda371578b5ba2e0
Johnathon <johnathonmgaines@gmail.com>
2025-04-26 11:56:01 -0500

    Added icons to the screene xample

--- examples/screen/screen.c
+++ examples/screen/screen.c
@@ -15,8 +15,12 @@ const double IMAGE_HEIGHT = 50;
 const double ICON_X_PADDING = 10;
 const double ICON_Y_PADDING = 10;
 const double ICON_WIDTH = 70;
-const double ICON_HEIGHT = 70;
+const double ICON_HEIGHT = 90;
+#define NUM_ICON_TEXT_LINES 3
+const char* ICON_TEXT_FONT = "12px Seriph";
+const unsigned long ICON_TEXT_COLOR = 0x000000FF;
 
+const double ICON_TEXT_IMAGE_SPACE = 5.0;
 const double IMAGE_X_OFFSET = (ICON_WIDTH - IMAGE_WIDTH)/2.0;
 const double IMAGE_Y_OFFSET = 0.0;
 
@@ -31,6 +35,8 @@ struct MainWindowEvents {
    int mouse_click_event_id;
    int mouse_double_click_event_id;
    int keyboard_press_event_id;
+
+   int canvas_resize_event_id;
 };
 struct MainWindowInfo {
    twr_ioconsole_t* window;
@@ -49,14 +55,14 @@ struct Icon {
    int initialized;
    const char* item_name;
    size_t num_text_lines;
-   struct IconTextLine text_lines[3];
+   struct IconTextLine text_lines[NUM_ICON_TEXT_LINES];
 };
 
 struct GlobalEvents {
    int icon_image_load_event_id;
 };
 
-#define NUM_ICONS 3
+#define NUM_ICONS 10
 struct GlobalState {
    twr_ioconsole_t* screen;
    struct MainWindowInfo main_window_info;
@@ -70,24 +76,103 @@ struct GlobalState global_state = {
    .initialized = FALSE,
    .objectID = 0,
 };
+char* sub_string(const char* str, size_t start, size_t end, size_t end_padding_len, char pad_char) {
+   assert(start < end);
+   char* res_str = (char*)malloc(end - start + 1 + end_padding_len);
+   size_t res_index = 0;
+   for (size_t i = start; i < end; i++) {
+      res_str[res_index] = str[i];
+      res_index++;
+   }
+   for (size_t i = 0; i < end_padding_len; i++) {
+      res_str[res_index] = pad_char;
+      res_index++;
+   }
+   res_str[res_index] = '\0';
+   return res_str;
+}
 void setup_icon_text(struct d2d_draw_seq* ds, struct Icon* icon) {
    for (size_t i = 0; i < icon->num_text_lines; i++) {
-      free(icon->text_lines[i].text);
+      free((void*)(icon->text_lines[i].text));
    }
+   d2d_setfont(ds, ICON_TEXT_FONT);
    char* main_text = strdup(icon->item_name);
    struct d2d_text_metrics metrics;
-   d2d_measuretext(ds, main_text, &metrics);
+   icon->num_text_lines = 1;
+   for (size_t i = 0; i < NUM_ICON_TEXT_LINES; i++) {
+      d2d_measuretext(ds, main_text, &metrics);
+      if (metrics.width <= ICON_WIDTH) {
+         icon->text_lines[i].text = main_text;
+         main_text = NULL;
+      } else {
+         char* new_line = NULL;
+         int completed = 0;
+         //attempt to split the string on a space
+         size_t str_len = strlen(main_text);
+         for (size_t j = str_len-1; j >= 0; j--) {
+            if (main_text[j] == ' ') {
+               char tmp = main_text[j];
+               main_text[j] = '\0';
+               d2d_measuretext(ds, main_text, &metrics);
+               main_text[j] = tmp;
+               if (metrics.width <= ICON_WIDTH) {
+                  completed = 1;
+                  //realloc the string to the reduced size
+                  new_line = sub_string(main_text, 0, j, 0, '\0');
+                  //do the same with the main_text to get the rest of the text
+                  char* tmp = sub_string(main_text, j+1, str_len, 0, '\0');
+                  free(main_text);
+                  main_text = tmp;
+                  break;
+               }
+            }
+         }
+         //if it failed to split on a space, split mid-word
+         if (!completed) {
+            //splits long words:
+            // Measurements:
+            //    Measure-
+            //    meants
+            for (size_t j = 0; main_text[j] != '\0'; j++) {
+               char tmp1 = main_text[j];
+               char tmp2 = main_text[j+1];
+               main_text[j] = '-';
+               main_text[j+1] = '\0';
+               d2d_measuretext(ds, main_text, &metrics);
+               main_text[j] = tmp1;
+               main_text[j+1] = tmp2;
+               if (metrics.width > ICON_WIDTH) {
+                  size_t split_index = j - 2;
+                  new_line = sub_string(main_text, 0, split_index, 1, '-');
+                  char* tmp = sub_string(main_text, split_index, str_len, 0, '\0');
+                  free(main_text);
+                  main_text = tmp;
+               }
+            }
+         }
+         icon->text_lines[i].text = new_line;
+      }
+      d2d_measuretext(ds, icon->text_lines[i].text, &metrics);
+      icon->text_lines[i].offset.x = (ICON_WIDTH - metrics.width)/2.0;
+      icon->text_lines[i].offset.y = IMAGE_HEIGHT + ICON_TEXT_IMAGE_SPACE + i*(ICON_HEIGHT - IMAGE_HEIGHT - ICON_TEXT_IMAGE_SPACE)/NUM_ICON_TEXT_LINES;
 
-   // for (size_t i = 0; i < )
-
+      if (main_text == NULL) break;
+      icon->num_text_lines++;
+   }
+   if (main_text != NULL) {
+      free(main_text);
+      main_text = NULL;
+   }
 }
 void init_icon(struct d2d_draw_seq* ds, struct Icon* icon, const char* image_src, const char* item_name) {
    *icon = (struct Icon) {
       .image_src = image_src,
       .image_id = global_state.objectID++,
       .initialized = 0,
-      .item_name = item_name
+      .item_name = item_name,
+      .num_text_lines = 0,
    };
+   setup_icon_text(ds, icon);
    d2d_load_image_sync_with_con(
       icon->image_src,
       icon->image_id,
@@ -114,8 +199,43 @@ int init() {
 
    main_window_info->events = (struct MainWindowEvents) {
       .animation_event_id = twr_register_callback("mainWindowAnimationLoop"),
+      .mouse_move_event_id = twr_register_callback("mainWindowMouseEvent"),
+      .mouse_click_event_id = twr_register_callback("mainWindowMouseEvent"),
+      .mouse_double_click_event_id = twr_register_callback("mainWindowMouseEvent"),
       .keyboard_press_event_id = twr_register_callback("mainWindowKeyboardEvent"),
+
+      .canvas_resize_event_id = twr_register_callback("mainWindowDrawCanvasResize"),
    };
+   d2d_register_event_with_con(
+      D2D_ANIMATION_FRAME,
+      main_window_info->events.animation_event_id,
+      main_window_info->canvas
+   );
+   d2d_register_event_with_con(
+      D2D_MOUSE_MOVE,
+      main_window_info->events.mouse_move_event_id,
+      main_window_info->canvas
+   );
+   d2d_register_event_with_con(
+      D2D_MOUSE_CLICK,
+      main_window_info->events.mouse_click_event_id,
+      main_window_info->canvas
+   );
+   d2d_register_event_with_con(
+      D2D_MOUSE_DBLCLICK,
+      main_window_info->events.mouse_double_click_event_id,
+      main_window_info->canvas
+   );
+   d2d_register_event_with_con(
+      D2D_KEY_DOWN,
+      main_window_info->events.keyboard_press_event_id,
+      main_window_info->canvas
+   );
+   d2d_register_event_with_con(
+      D2D_CANVAS_RESIZE,
+      main_window_info->events.canvas_resize_event_id,
+      main_window_info->canvas
+   );
 
    main_window_info->canvas_size = (struct dVec2){
       .x = io_get_prop(main_window_info->canvas, "canvasWidth"),
@@ -132,6 +252,9 @@ int init() {
    init_icon(ds, &icons[0], "icons/pong.png", "Pong");
    init_icon(ds, &icons[1], "icons/window_example.png", "Window Example");
    init_icon(ds, &icons[2], "icons/app_opener.png", "App Opener");
+   for (size_t i = 3; i < NUM_ICONS; i++) {
+      init_icon(ds, &icons[i], "icons/app_opener.png", "App Opener");
+   }
 
    d2d_end_draw_sequence(ds);
    
@@ -140,7 +263,7 @@ int init() {
 }
 
 __attribute__((export_name("mainWindowDrawCanvasResize")))
-int main_window_draw_canvas_resize(int event_id, int width, int height) {
+void main_window_draw_canvas_resize(int event_id, int width, int height) {
    global_state.main_window_info.canvas_size = (struct dVec2){
       .x = width,
       .y = height
@@ -155,7 +278,6 @@ void icon_image_load_event(int event_id, int success, struct Icon* icon) {
       printf("Warning: failed to load image %s\n", icon->image_src);
    }
 }
-
 __attribute__((export_name("mainWindowAnimationLoop")))
 void main_window_animation_loop(int event_id, int delta_t) {
    struct d2d_draw_seq* ds = d2d_start_draw_sequence_with_con(100, global_state.main_window_info.canvas);
@@ -166,26 +288,64 @@ void main_window_animation_loop(int event_id, int delta_t) {
    d2d_fillrect(ds, 0, 0, canvas_size.x, canvas_size.y);
 
 
-   int icons_per_row = ceil(canvas_size.x/ICON_WIDTH);
-
    int row = 0;
    int column = 0;
    struct Icon* icons = global_state.icons;
-   for (int i = 0; i < NUM_ICONS; i++) {
-      double x = row*ICON_WIDTH + (row-1)*ICON_X_PADDING;
-      double y = column*ICON_HEIGHT + (column-1)*ICON_Y_PADDING;
+   for (size_t i = 0; i < NUM_ICONS; i++) {
+      double x = row*(ICON_WIDTH + ICON_X_PADDING);
+      if (x + ICON_WIDTH > global_state.main_window_info.canvas_size.x && row != 0) {
+         row = 0;
+         column++;
+         x = row*(ICON_WIDTH + ICON_X_PADDING);
+      }
+      double y = column*(ICON_HEIGHT + ICON_Y_PADDING);
+      row++;
 
-      d2d_drawimage_ex(ds, icons[i].image_id, 0, 0, 0, 0, x+IMAGE_X_OFFSET, y+IMAGE_Y_OFFSET, IMAGE_WIDTH, IMAGE_HEIGHT);
+      if (icons[i].initialized) {
+         d2d_drawimage_ex(ds, icons[i].image_id, 0, 0, 0, 0, x+IMAGE_X_OFFSET, y+IMAGE_Y_OFFSET, IMAGE_WIDTH, IMAGE_HEIGHT);
+      } else {
+         d2d_setfillstylergba(ds, 0xFF0000FF);
+         d2d_fillrect(ds, x + IMAGE_X_OFFSET, y + IMAGE_Y_OFFSET, IMAGE_WIDTH, IMAGE_HEIGHT);
+      }
 
-
+      d2d_setfont(ds, ICON_TEXT_FONT);
+      d2d_setfillstylergba(ds, ICON_TEXT_COLOR);
+      
+      d2d_setcanvaspropstring(ds, "textBaseline", "top");
+      for (size_t j = 0; j < icons[i].num_text_lines; j++) {
+         d2d_filltext(
+            ds,
+            icons[i].text_lines[j].text,
+            x + icons[i].text_lines[j].offset.x,
+            y + icons[i].text_lines[j].offset.y
+         );
+      }
    }
 
    d2d_end_draw_sequence(ds);
 }
 
 __attribute__((export_name("mainWindowMouseEvent")))
-void main_window_mouse_event(int event_id, int x, int y, int button) {
+void main_window_mouse_event(int event_id, int mouse_x, int mouse_y, int button) {
+   int row = 0;
+   int column = 0;
+   for (size_t i = 0; i < NUM_ICONS; i++) {
+      double x = row*(ICON_WIDTH + ICON_X_PADDING);
+      if (x + ICON_WIDTH > global_state.main_window_info.canvas_size.x && row != 0) {
+         row = 0;
+         column++;
+         x = row*(ICON_WIDTH + ICON_X_PADDING);
+      }
+      double y = column*(ICON_HEIGHT + ICON_Y_PADDING);
+      row++;
 
+      if (
+         x <= mouse_x && mouse_x <= x+ICON_WIDTH
+         && y <= mouse_y && mouse_y <= y+ICON_HEIGHT
+      ) {
+         printf("Clicked on: %s\n", global_state.icons[i].item_name);
+      }
+   }
 }
 
 __attribute__((export_name("mainWindowKeyboardEvent")))
